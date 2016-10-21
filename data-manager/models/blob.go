cbb92b3389da84678ab075a657f470d0e3514a0a
lvalerio <lvaleriocasti@vmware.com>
2016-10-20 22:35:12 -0500

    Added to JSON and from JSON functions

--- data-manager/models/blob.go
+++ data-manager/models/blob.go
@@ -7,11 +7,33 @@
 //
 package models
 
+import (
+	"encoding/json"
+)
+
 // Blob is a struct used to store data into the blob service.
 type Blob struct {
-	ID      int     `json:"id"`
-	Version string  `json:"version"`
-	Name    string  `json:"name"`
-	Content Content `json:"content"`
-	Tag     string  `json:"tag"`
+	ID      int    `json:"id"`
+	Version string `json:"version"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+	Tag     string `json:"tag"`
+}
+
+// FromJSON populates Reservation from JSON data.
+func (b *Blob) FromJSON(bytes []byte) error {
+	err := json.Unmarshal(bytes, b)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ToJSON returns a JSON formatted string representation of the Reservation.
+func (b *Blob) ToJSON() (string, error) {
+	blobJson, err := json.Marshal(b)
+	if err != nil {
+		return "", err
+	}
+	return string(blobJson), nil
 }
